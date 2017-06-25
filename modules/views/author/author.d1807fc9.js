24f66da3183059251fc82d786973ff3162d44da4
zhoucs <zhoucs@zhoucsdeMacBook-Pro.local>
2017-06-25 23:15:03 +0800

    delete old file

--- modules/views/author/author.d1807fc9.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([9],{45:function(t,o){t.exports='<div layout="row"   layout-padding>    <md-content md-scroll-y layout="column" flex>        <div ng-bind-html="authorInfo|TrustHtmlFilter"></div>    </md-content></div>'}});
