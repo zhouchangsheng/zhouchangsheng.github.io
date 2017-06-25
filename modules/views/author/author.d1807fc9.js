d46bbc4e75e296942de0595d9c594fd3821e09fb
zhoucs <zhoucs@zhoucsdeMacBook-Pro.local>
2017-06-25 23:15:33 +0800

    fix changyan

---
+++ modules/views/author/author.d1807fc9.js
@@ -0,0 +1,1 @@
+webpackJsonp([9],{45:function(t,o){t.exports='<div layout="row"   layout-padding>    <md-content md-scroll-y layout="column" flex>        <div ng-bind-html="authorInfo|TrustHtmlFilter"></div>    </md-content></div>'}});
